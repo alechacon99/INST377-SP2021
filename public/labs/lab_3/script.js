d047efde588e4cb4326b5a1510c3fbe22a2b578a
alechacon99 <77997217+alechacon99@users.noreply.github.com>
2021-02-12 21:19:36 -0500

    Lab 3 & fixes to lab 1

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,39 @@
-/* Put your javascript in here */
+const slideArray = [];
+for (let i = 0; i < document.querySelectorAll('body img').length; i++) {
+  slideArray.push(document.querySelectorAll('body img')[i].src);
+};
+console.log(slideArray);
+
+let currentSlideIndex = 0;
+
+function nextImage() {
+    const nextArrow = document.querySelector('#next');
+nextArrow.addEventListener('click', (event) => {
+    currentSlideIndex++;
+
+    if (currentSlideIndex+2 >= slideArray.length) {
+        currentSlideIndex = 4;
+    }
+    
+    document.querySelector('.img_bay_1').style.cssText = 'background: url("' + slideArray[currentSlideIndex] + '") no-repeat center center; background-size: cover;';
+    document.querySelector('.img_bay_2').style.cssText = 'background: url("' + slideArray[currentSlideIndex+1] + '") no-repeat center center; background-size: cover;';
+    document.querySelector('.img_bay_3').style.cssText = 'background: url("' + slideArray[currentSlideIndex+2] + '") no-repeat center center; background-size: cover;';
+});
+};
+
+function prevImage() {
+    const prevArrow = document.querySelector('#prev')
+prevArrow.addEventListener('click', (event) => {
+    currentSlideIndex--;
+
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = 0;
+    }
+    document.querySelector('.img_bay_1').style.cssText = 'background: url("' + slideArray[currentSlideIndex] + '") no-repeat center center; background-size: cover;';
+    document.querySelector('.img_bay_2').style.cssText = 'background: url("' + slideArray[currentSlideIndex+1] + '") no-repeat center center; background-size: cover;';
+    document.querySelector('.img_bay_3').style.cssText = 'background: url("' + slideArray[currentSlideIndex+2] + '") no-repeat center center; background-size: cover;';
+});
+};
+
+nextImage();
+prevImage();
